148f130f7eaee38aa7a9774f9e11b81eb2ca8d41
Igor Fedyukin <igor_fedyukin@epam.com>
2017-12-04 17:55:19 +0300

    Fixed webpack build fail

--- src/client/components/posts/PostsList.jsx
+++ src/client/components/posts/PostsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Post } from './Post';
 
-export const PostsList = ({ posts }) => (
- // вывести "массив" компонентов-записей
+export const PostsList = ({ posts }) => (null
+ // убрать null и вывести "массив" компонентов-записей
 );
