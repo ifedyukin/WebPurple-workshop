37c80cbe9dbf8bc06955626f728ed2afa8ed18a5
Igor Fedyukin <igor_fedyukin@epam.com>
2017-12-04 19:11:14 +0300

    Fixed build

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,5 +1,4 @@
 import mongoose, { Schema, mongo } from 'mongoose';
-import bcrypt from 'bcrypt-as-promised';
 
 const UserSchema = new Schema({
   login: { type: String, unique: true, lowercase: true, index: true, required: true },
@@ -11,15 +10,11 @@ UserSchema.pre('save', async function (next) {
     return next();
   }
 
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(this.password, salt);
-
-  this.password = hash;
   next();
 });
 
 UserSchema.methods.comparePasswords = function (password) {
-  return bcrypt.compare(password, this.password);
+  return password === this.password;
 }
 
 export default mongoose.model('User', UserSchema);
