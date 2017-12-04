27146e213215ee5bad86a443897a50824da5af5a
Igor Fedyukin <igor_fedyukin@epam.com>
2017-12-04 19:02:29 +0300

    Fixed build

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,28 +1,20 @@
 import mongoose, { Schema, mongo } from 'mongoose';
-import bcrypt from 'bcrypt-as-promised';
 
-// Описываем схему пользователя
 const UserSchema = new Schema({
   login: { type: String, unique: true, lowercase: true, index: true, required: true },
   password: { type: String, required: true }
 });
 
-// Перед сохранением мы хешируем пароль пользователя (это вопрос безопасности)
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     return next();
   }
 
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(this.password, salt);
-
-  this.password = hash;
   next();
 });
 
-// Описание метода модели, позволяющего проверить, совпадают ли пароли
 UserSchema.methods.comparePasswords = function (password) {
-  return bcrypt.compare(password, this.password);
+  return password === this.password;
 }
 
 export default mongoose.model('User', UserSchema);
